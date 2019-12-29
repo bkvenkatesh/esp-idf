6a397304c6506acf226aba25fa05b0d83a60f63b
Venkatesh Bachu <bkvenkatesh@hotmail.com>
2019-12-29 09:33:12 +0100

    Adding env setup bashScript

---
+++ start_esp.sh
@@ -0,0 +1,1 @@
+export PATH=$PATH:'C:\workspace\esp-idf-tools\xtensa-esp32-elf-gcc8_2_0-esp32-2019r1-win32\xtensa-esp32-elf\bin':'C:\workspace\esp-idf-tools\ninja-win':'C:\workspace\esp-idf-tools\mconf-v4.6.0.0-idf-20190628-win32'
